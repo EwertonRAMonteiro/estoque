f52eaba44b3b1117ef5f047889ddc68206b1da6d
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2025-06-13 12:12:40 -0300

    adicionando anotações para entidade User

--- src/main/java/com/rubi/sistemaestoque/entities/User.java
+++ src/main/java/com/rubi/sistemaestoque/entities/User.java
@@ -1,15 +1,41 @@
 package com.rubi.sistemaestoque.entities;
 
 import com.rubi.sistemaestoque.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
-    Long id;
-    String name;
-    String midleName;
-    String lastName;
-    String cpf;
-    String username;
-    String senha;
-    Role role;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fisrtName", nullable = false, length = 30)
+    private String name;
+
+    @Column( name = "midName", length = 30)
+    private String midName;
+
+    @Column(name = "lastName",nullable = false, length = 30)
+    private String lastName;
+
+    @Column(name = "userName", nullable = false, length = 30, unique = true)
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
 
 }
