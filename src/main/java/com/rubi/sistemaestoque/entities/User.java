0c009d1105689969a6b1bd28854530bb3f79ee6a
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2025-06-13 11:00:44 -0300

    criando enum de role e entidade usuario e dependencias para o sistema

---
+++ src/main/java/com/rubi/sistemaestoque/entities/User.java
@@ -0,0 +1,15 @@
+package com.rubi.sistemaestoque.entities;
+
+import com.rubi.sistemaestoque.enums.Role;
+
+public class User {
+    Long id;
+    String name;
+    String midleName;
+    String lastName;
+    String cpf;
+    String username;
+    String senha;
+    Role role;
+
+}
